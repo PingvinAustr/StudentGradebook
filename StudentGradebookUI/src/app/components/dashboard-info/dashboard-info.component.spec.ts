e80f4c957707bdfc0d57cd41039168549a84bd42
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2024-05-08 09:27:37 +0300

    Minor config update 2

--- StudentGradebookUI/src/app/components/dashboard-info/dashboard-info.component.spec.ts
+++ StudentGradebookUI/src/app/components/dashboard-info/dashboard-info.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DashboardInfoComponent } from './dashboard-info.component';
 
@@ -6,7 +6,7 @@ describe('DashboardInfoComponent', () => {
   let component: DashboardInfoComponent;
   let fixture: ComponentFixture<DashboardInfoComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardInfoComponent ]
     })
